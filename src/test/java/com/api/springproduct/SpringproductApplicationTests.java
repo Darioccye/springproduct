9a1d6c7b07006a5ebf7fa166dcce9392c211fdb9
Dario Chen <dariochenchenye@gmail.com>
2024-06-01 17:24:18 -0300

    Começando o projeto

---
+++ src/test/java/com/api/springproduct/SpringproductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.springproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringproductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
